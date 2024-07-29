07f4669db6969fade0388319805017991199005a
Tanay Mahindru <tanay.mahindru@protonmail.com>
2024-07-29 08:57:33 -0700

    cookie-test-11

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
       )}
     >
       <head>
-      <meta http-equiv="Content-Security-Policy" content="default-src 'self' data: gap: https://ssl.gstatic.com 'unsafe-eval'; style-src 'self' 'unsafe-inline'; media-src *; script-src 'self' cdn.cookielaw.org http://onlineerp.solution.quebec 'unsafe-inline' 'unsafe-eval';" />
+      <meta http-equiv="Content-Security-Policy" content="default-src 'self' data: gap: https://ssl.gstatic.com 'unsafe-eval'; style-src 'self' 'unsafe-inline'; media-src *; script-src 'self' https://cdn.cookielaw.org http://onlineerp.solution.quebec 'unsafe-inline' 'unsafe-eval';" />
     
     <script src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js" type="text/javascript" charSet="UTF-8" data-domain-script="0190e591-2d31-79cf-aa8c-8bf84f9788a9-test"></script>
     <script type="text/javascript">
