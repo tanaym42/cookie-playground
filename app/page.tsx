24aa0734006260b140511b65e887362c05a651d0
Tanay Mahindru <tanay.mahindru@protonmail.com>
2024-08-01 13:01:55 -0700

    embed-test-1

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ export default function Page() {
       <p className="prose prose-neutral dark:prose-invert">
         This is your new portfolio.
       </p>
+
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/4_9UbOxoNMM?si=MBlpMaxWMzpFxCzk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </section>
   );
 }
