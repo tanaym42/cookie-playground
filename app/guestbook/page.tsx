7947ca62f756efd487d99e59143bd717884238b9
Tanay Mahindru <tanay.mahindru@protonmail.com>
2024-08-01 13:46:22 -0700

    embed-test-5

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -19,6 +19,7 @@ export default function GuestbookPage() {
         <GuestbookForm />
         <GuestbookEntries />
       </Suspense>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/4_9UbOxoNMM?si=MBlpMaxWMzpFxCzk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     </section>
   );
 }
