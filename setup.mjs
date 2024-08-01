ad7237a19095f9c71610075c152e96f5d0fc6929
Tanay Mahindru <tanay.mahindru@protonmail.com>
2024-08-01 16:17:26 -0700

    embed-test-7

--- setup.mjs
+++ setup.mjs
@@ -13,10 +13,11 @@ Hello, World!`;
 const homePage = `export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my portfolio</h1>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">This is a website to test cookie banner implementation</h1>
       <p className="prose prose-neutral dark:prose-invert">
-        This is your new portfolio.
+        This is a cookie banner test (and embedded videos as well)
       </p>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/4_9UbOxoNMM?si=MBlpMaxWMzpFxCzk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     </section>
   );
 }
