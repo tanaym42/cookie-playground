00221a4e4b21e059e478afde13f2214c80e1c4e4
Tanay Mahindru <tanay.mahindru@protonmail.com>
2024-08-01 13:44:03 -0700

    text-edit-1

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -13,7 +13,7 @@ export default function GuestbookPage() {
   return (
     <section>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">
-        sign my guestbook
+        don't sign my guestbook
       </h1>
       <Suspense>
         <GuestbookForm />
